a2036c20f5cbeaa98854f828c7be2f8d9825ce4f
li-lauren <laurenlidev@gmail.com>
2020-10-08 17:17:51 -0600

    Finish Part 2

--- harvest.py
+++ harvest.py
@@ -79,16 +79,53 @@ class Melon(object):
 
     # Fill in the rest
     # Needs __init__ and is_sellable methods
+    def __init__(self, melon_type, shape_rating, color_rating, harvest_from, harvest_by):
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.harvest_from = harvest_from
+        self.harvest_by = harvest_by
+
+    def is_sellable(self):
+        if self.shape_rating > 5 and self.color_rating > 5 and self.harvest_from != 3:
+            return True
+        else:
+            return False 
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    melons = []
+
+    melons_by_id = make_melon_type_lookup(melon_types)
+    melon_1 = Melon(melons_by_id['yw'], 8, 7, 2, 'Sheila')
+    melon_2 = Melon(melons_by_id['yw'], 3, 4, 2, 'Sheila')
+    melon_3 = Melon(melons_by_id['yw'], 9, 8, 3, 'Sheila')
+    melon_4 = Melon(melons_by_id['cas'], 10, 6, 35, 'Sheila')
+    melon_5 = Melon(melons_by_id['cren'], 8, 9, 35, 'Michael')
+    melon_6 = Melon(melons_by_id['cren'], 8, 2, 35, "Michael")
+    melon_7 = Melon(melons_by_id['cren'], 2, 3, 4, "Michael")
+    melon_8 = Melon(melons_by_id['musk'], 6, 7, 4, "Michael")
+    melon_9 = Melon(melons_by_id['yw'], 7, 10, 3, "Sheila")
+
+    melons = [melon_1, melon_2, melon_3, melon_4, melon_5, melon_6, melon_7, 
+    melon_8, melon_9]
+
+    return melons
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
+    for melon in melons:
+        if melon.is_sellable():
+            print(f"Harvested by {melon.harvest_by} from Field {melon.harvest_from} (CAN BE SOLD)")
+        else:
+            print(f"Harvested by {melon.harvest_by} from Field {melon.harvest_from} (NOT SELLABLE)")
+
+melon_types = make_melon_types()
+melons = make_melons(melon_types)
+print(melons)
+print(get_sellability_report(melons))
 
 
 
